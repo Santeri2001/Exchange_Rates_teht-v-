db367d99aa8019592161bfa6ead0420386f20837
Santonen <77671732+Santonen@users.noreply.github.com>
2022-10-17 00:49:52 +0300

    master

--- src/App.js
+++ src/App.js
@@ -7,6 +7,44 @@ function App() {
   const [eur,setEur] = useState(0);
   const [gbp,setGbp] = useState(0);
   const [rate,setRate] = useState(0);
+
+  return (
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>Eur</label>&nbsp;
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)}></input>
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Gbp</label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
+    </div>
+  );
+
 }
 
+async function convert(e) {
+  e.preventDefault();
+  try {
+    const address = URL;
+    const response = await fetch(address);
 
+    if (response.ok) {
+      const json = await response.json();
+      setRate(json.rates.GBP);
+      setGbp(eur * json.rates.GBP);
+    } else {
+      alert('Error retrieving exchange rate.');
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+
+export default App;
