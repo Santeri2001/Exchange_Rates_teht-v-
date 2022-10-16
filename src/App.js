d68c69598442ecc8278fa8ddb9c8cd35ae5de313
Santonen <77671732+Santonen@users.noreply.github.com>
2022-10-17 00:49:28 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const URL = 'https://api.exchangerate.host/latest'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [eur,setEur] = useState(0);
+  const [gbp,setGbp] = useState(0);
+  const [rate,setRate] = useState(0);
 }
 
-export default App;
+
